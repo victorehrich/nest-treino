139acd571cf2d02a0a2ca70df177d3ea3d5e6425
Victor Ehrich Carneiro de Medeiros <victorehrich@gmail.com>
2023-04-25 12:54:58 -0300

    deploying confs

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
+
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
@@ -17,6 +19,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/swagger', app, document);
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
